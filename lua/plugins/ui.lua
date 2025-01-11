914efe2ac04e4b5822fc577cff704a9e210c9f70
Pavel Hrakovich <pgrekovich@gmail.com>
2025-01-11 14:19:48 +0100

    feat(plugins): change mode for bufferline

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -85,7 +85,6 @@ return {
     event = "VeryLazy",
     opts = {
       options = {
-        mode = "tabs",
         -- separator_style = "slant",
         show_buffer_close_icons = false,
         show_close_icon = false,
